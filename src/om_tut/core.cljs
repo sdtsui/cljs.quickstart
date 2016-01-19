bc9a5ac23bd5747b235f52f31682a79b2727c0f8
sdtsui <danielt213@gmail.com>
2016-01-18 20:22:34 -0800

    init - Next: 'dealing with input fields

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,103 @@
+(ns ^:figwheel-always om-tut.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.string :as string]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+        (dom/span nil (display-name contact))
+        (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+
+(comment 
+(defn add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      ((om/transact! data :contacts #(conj % new-contact))))))
+)
+
+
+;; This works great, but any time you find yourself leaning heavily on refs it's probably worth stepping back and considering whether the same thing could be accomplished in a more declarative manner.
+(defn add-contact [data owner]
+  (let [input (om/get-node owner "new-contact")
+        new-contact (-> input .-value parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact))
+      (set! (.-value input) ""))))
+
+;; # Next: Dealing with text input fields
+
+
+
+(defn contacts-view [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+              (let [contact (<! delete)]
+                (om/transact! data :contacts
+                  (fn [xs] (vec (remove #(= contact %) xs))))
+                (recur))))))
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts data)
+            {:init-state state}))
+        (dom/div nil
+          (dom/input #js {:type "text" :ref "new-contact"})
+          (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
